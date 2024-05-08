3e90999c2b81e76512f3fcd628b8c3be1cf33f0c
LimMeiXuan1 <168528865+LimMeiXuan1@users.noreply.github.com>
2024-05-08 13:24:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    'Hi，这里是BlockPioneers，Hi，我们在创新的疆界，为你构建未来的游戏，Hi，我们的区块链解决方案，让你的业务更有价值，Hi，用我们的技术，为你的想象力插上翅膀，Hi，在这个数字世界中，我们用技术为你打开新的可能🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
